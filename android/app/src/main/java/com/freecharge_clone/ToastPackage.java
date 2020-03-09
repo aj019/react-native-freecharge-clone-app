dc3a95435e8c3ead886e5692c9dcd5ba356557d0
Anuj Gupta <junagupta19@gmail.com>
2020-03-09 21:44:24 +0530

    Added native module ToastModule that allows us to show Toast message in Android App

---
+++ android/app/src/main/java/com/freecharge_clone/ToastPackage.java
@@ -0,0 +1,31 @@
+package com.freecharge_clone;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import com.freecharge_clone.ToastModule;
+
+
+public class ToastPackage implements ReactPackage {
+
+    @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(
+                              ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new ToastModule(reactContext));
+
+    return modules;
+  }
+
+}
