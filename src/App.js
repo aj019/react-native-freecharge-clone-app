bbb39155c5290ba791b84834f3c4e56d194ac413
Anuj Gupta <junagupta19@gmail.com>
2020-03-18 13:21:36 +0530

    Added React Navigation package to enable navigation and to implement botton bar with icons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,83 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
+import {StyleSheet, Image, StatusBar} from 'react-native';
 import {Home, Reminders, PayMerchants, Account, Offers} from './containers';
 import TabView from 'react-native-scrollable-tab-view';
 import Colors from './constants/colors';
 import store from './store';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import pay from './images/pay.png';
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <Provider store={store}>
         <StatusBar
           backgroundColor={Colors.primaryColor}
           barStyle="light-content"
         />
-        <TabView
-          style={styles.bottomBar}
-          tabBarTextStyle={styles.tabText}
-          tabBarActiveTextColor={Colors.primaryColor}
-          tabBarInActiveTextColor="#000"
-          tabBarUnderlineStyle={{
-            width: 0,
-          }}
-          locked={true}
-          tabBarPosition="bottom">
-          <Home tabLabel="Home" />
-          <Reminders tabLabel="Reminders" />
-          <PayMerchants tabLabel="Pay Merchant" />
-          <Offers tabLabel="Offers" />
-          <Account tabLabel="Account" />
-        </TabView>
+
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: Colors.primaryColor,
+          }}>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({color, size}) => {
+                return <Icon name="home" color={color} size={size} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Reminders"
+            component={Reminders}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({color, size}) => {
+                return <Icon name="clock-o" color={color} size={size} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Pay Merchants"
+            component={PayMerchants}
+            options={{
+              tabBarLabel: 'Pay Merchants',
+              tabBarIcon: ({color, size}) => {
+                return <Icon name="qrcode" color={color} size={size} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Offers"
+            component={Offers}
+            options={{
+              tabBarLabel: 'Offers',
+              tabBarIcon: ({color, size}) => {
+                return <Icon name="ticket" color={color} size={size} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Account"
+            component={Account}
+            options={{
+              tabBarLabel: 'Account',
+              tabBarIcon: ({color, size}) => {
+                return <Icon name="user" color={color} size={size} />;
+              },
+            }}
+          />
+        </Tab.Navigator>
       </Provider>
-    </>
+    </NavigationContainer>
   );
 }
 
